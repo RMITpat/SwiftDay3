152e567dcfdeb73a0c6e5511d6cb950d2a3d365f
Patrick Ly <s4092047@student.rmit.edu.au>
2025-02-20 10:51:06 +1100

    Initial Commit

---
+++ SwiftDay3/SwiftDay3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDay3App.swift
+//  SwiftDay3
+//
+//  Created by Patrick Ly on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDay3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
