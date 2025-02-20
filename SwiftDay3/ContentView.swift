d6f1be9073ec025dbec20d9127deda358a682963
Patrick Ly <s4092047@student.rmit.edu.au>
2025-02-20 11:28:15 +1100

    made the calculator

--- SwiftDay3/ContentView.swift
+++ SwiftDay3/ContentView.swift
@@ -6,14 +6,43 @@
 //
 
 import SwiftUI
+func calculateDogYears(age: Double) -> Double {
+    return age * 7
+}
 
 struct ContentView: View {
+    
+    @State private var age = 0.0
+    
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+       
+            
+            switch age {
+            case 0...15:
+                Text("🐶")
+                    .font(.system(size : 200))
+            case 16...25:
+                Text("🪦")
+                    .font(.system(size : 200))
+            
+            default:
+                Text("😱")
+                    .font(.system(size : 200))
+            }
+            
+            Text("Dog Years Calculator!")
+                .font(.largeTitle)
+                .fontDesign(.rounded)
+                .fontWeight(.black)
+            Slider(value: $age, in: 0...25, step : 1)
+            Text("\(Int(age))")
+                .font(.title2)
+            Text("Dog age: \(Int(calculateDogYears(age: age)))")
+                .font(.title2)
+            
         }
         .padding()
     }
